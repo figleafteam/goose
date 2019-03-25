e17f9fad686292a733396dc4586a16fec828c9aa
Leonid Shylin <lonja007@gmail.com>
2019-03-25 08:18:12 +0200

    Implement apply all migrations command

--- cmd/goose/main.go
+++ cmd/goose/main.go
@@ -120,15 +120,17 @@ Options:
 
 	usageCommands = `
 Commands:
-    up                   Migrate the DB to the most recent version available
-    up-to VERSION        Migrate the DB to a specific VERSION
-    down                 Roll back the version by 1
-    down-to VERSION      Roll back to a specific VERSION
-    redo                 Re-run the latest migration
-    reset                Roll back all migrations
-    status               Dump the migration status for the current DB
-    version              Print the current version of the database
-    create NAME [sql|go] Creates new migration file with the current timestamp
-    fix                  Apply sequential ordering to migrations
+    up					   Migrate the DB to the most recent version available ignoring unapplied versions < current
+	up-all-unapplied [fix] Migrate the DB to the most recent version available applying all unapplied migrations.
+						   Fix option allows to fix applied migrations ordering
+    up-to VERSION          Migrate the DB to a specific VERSION
+    down                   Roll back the version by 1
+    down-to VERSION        Roll back to a specific VERSION
+    redo                   Re-run the latest migration
+    reset                  Roll back all migrations
+    status                 Dump the migration status for the current DB
+    version                Print the current version of the database
+    create NAME [sql|go]   Creates new migration file with the current timestamp
+    fix                    Apply sequential ordering to migrations
 `
 )
